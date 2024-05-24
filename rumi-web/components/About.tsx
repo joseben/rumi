563f7799d9b1f52810e891f1ff2f1a85533bdcb7
Jose Ben <36511657+joseben@users.noreply.github.com>
2024-05-24 13:07:30 +0530

    Update About.tsx

--- rumi-web/components/About.tsx
+++ rumi-web/components/About.tsx
@@ -1,3 +1,4 @@
+import { ArrowDownTrayIcon } from '@heroicons/react/16/solid'
 import React from 'react'
 
 const About = () => {
@@ -17,6 +18,10 @@ const About = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis perferendis, corporis asperiores ullam odio saepe consequuntur laboriosam quo facilis, laborum nisi? Fuga.
                     </p>
                 </div>
+                <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2'>
+                        <p>Download doc</p>
+                        <ArrowDownTrayIcon className='w-[1.6rem] h-[1.7rem] text-black '/>
+                </button>
             </div>
         </div>
     </div>
