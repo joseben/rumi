825851b43a32806cd3d99082cecd06b1172a111a
Jose Ben <36511657+joseben@users.noreply.github.com>
2024-06-01 19:31:17 +0530

    same as server

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Nav from "@/components/Nav"
+import MobileNav from '@/components/MobileNav'
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Projects from '@/components/Projects'
+
+const HomePage = () => {
+  const [nav,setnav]=useState(false)
+  const openNav =()=> setnav(true)
+  const closeNav =()=> setnav(false)
+
+  return (
+    <div className='overflow-x-hidden'>
+      <div>
+        {/* Navbar */}
+        <MobileNav nav={nav} closeNav={closeNav}/>
+        <Nav openNav={openNav}/>
+        {/* Hero section */}
+        <Hero/>
+        <div className='relative z-[30]'>
+          <About/>
+           {/* Projects */}
+           <Projects/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
