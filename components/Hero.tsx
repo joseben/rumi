bb0bd911dc1c3b0b19256b7fa4c94dc9b685020b
Jose Ben <36511657+joseben@users.noreply.github.com>
2024-12-20 16:41:07 -0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
                 </h1>
                 <TextEffect/>
                 <p className='mt-[1.5rem] text-[18px] text-[#ffffff92] '>
-                    Enne patti thallan eniku vere oru thendiyudem avishyam illa. 
+                Kaanaan oru look illanney ullu… bhayankara budhiya.
                 </p>
                 <div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
                     <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2'>
